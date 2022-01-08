4467b0d481d05e23140b7726a7617d758dc0e953
yamrire <yamrire@student.1337.ma>
2022-01-08 18:27:45 +0100

    Updating to one Header File

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 14:03:27 by yamrire           #+#    #+#             */
-/*   Updated: 2022/01/08 14:07:51 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/01/08 18:19:03 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,14 @@
 #define LIBFTPRINTF_H
 
 #include <stdarg.h>
-#include "libft/libft.h"
+#include <unistd.h>
 
-void ft_nbr_base (unsigned long long n, int base);
-void ft_nbr_gbase (unsigned long long n, int base);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putnbrunsign_fd(unsigned long long n, int fd);
+void	ft_putstr_fd(char *s, int fd);
+int     ft_printf(const char *str, ...);
+void    handle_print_cases(const char *str, int i, va_list format);
+void    ft_nbr_base (unsigned long long n, int base, int);
 
 #endif
