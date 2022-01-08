5867ccd52cd219fcc0701e8385f22c7898f68c62
yamrire <yamrire@student.1337.ma>
2022-01-08 13:32:59 +0100

    Correction of ft_putnbrunsign_fd

--- libft/ft_putnbrunsign_fd.c
+++ libft/ft_putnbrunsign_fd.c
@@ -6,7 +6,7 @@
 /*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 18:18:52 by yamrire           #+#    #+#             */
-/*   Updated: 2022/01/05 18:46:49 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/01/08 13:31:19 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,6 @@
 
 void	ft_putnbrunsign_fd(unsigned long long n, int fd)
 {
-	if (n < 0)
-	{
-		n *= -1;
-		ft_putchar_fd('-', fd);
-	}
 	if (n / 10)
 		ft_putnbrunsign_fd (n / 10, fd);
 	ft_putchar_fd (n % 10 + '0', fd);
