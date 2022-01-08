399d72ef79be571d955d9d23cfd0028207e3ce8e
yamrire <yamrire@student.1337.ma>
2022-01-08 18:26:01 +0100

    Updating ft_nbr_base to one function

--- ft_nbr_base.c
+++ ft_nbr_base.c
@@ -6,24 +6,20 @@
 /*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 18:25:16 by yamrire           #+#    #+#             */
-/*   Updated: 2022/01/05 18:42:45 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/01/08 18:18:18 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
+#include "libftprintf.h"
 
-void ft_nbr_base (unsigned long long n, int base)
+void ft_nbr_base (unsigned long long n, int base, int which)
 {
-	const char *bb = "0123456789abcdef";
+	char *bb;
+	if (which)
+		bb = "0123456789ABCDEF";
+	else
+		bb = "0123456789abcdef";
 	if (n / base)
-		ft_nbr_base (n / base, base);
-	ft_putchar_fd (bb[n % base], 1);
-}
-
-void ft_nbr_gbase (unsigned long long n, int base)
-{
-	const char *bb = "0123456789ABCDEF";
-	if (n / base)
-		ft_nbr_gbase (n / base, base);
+		ft_nbr_base (n / base, base, which);
 	ft_putchar_fd (bb[n % base], 1);
 }
