fb818f28b9b65284ac03c77c3514a0bdbe746674
yamrire <yamrire@student.1337.ma>
2022-01-10 15:21:12 +0100

    Change ft_putnbr_fd param int > long

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,23 +6,21 @@
 /*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 23:44:51 by yamrire           #+#    #+#             */
-/*   Updated: 2022/01/08 18:18:37 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/01/10 15:11:39 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_putnbr_fd(long n, int fd)
 {
-	long	m;
 
-	m = n;
-	if (m < 0)
+	if (n < 0)
 	{
-		m = -m;
+		n *= -1;
 		ft_putchar_fd('-', fd);
 	}
-	if (m / 10)
-		ft_putnbr_fd (m / 10, fd);
-	ft_putchar_fd (m % 10 + '0', fd);
+	if (n / 10)
+		ft_putnbr_fd (n / 10, fd);
+	ft_putchar_fd (n % 10 + '0', fd);
 }
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 14:03:27 by yamrire           #+#    #+#             */
-/*   Updated: 2022/01/08 18:19:03 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/01/10 15:12:22 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@
 #include <unistd.h>
 
 void	ft_putchar_fd(char c, int fd);
-void	ft_putnbr_fd(int n, int fd);
+void	ft_putnbr_fd(long n, int fd);
 void	ft_putnbrunsign_fd(unsigned long long n, int fd);
 void	ft_putstr_fd(char *s, int fd);
 int     ft_printf(const char *str, ...);
-void    handle_print_cases(const char *str, int i, va_list format);
 void    ft_nbr_base (unsigned long long n, int base, int);
 
 #endif
