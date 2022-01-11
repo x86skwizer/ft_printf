6bf356657ff5e30682dae354fc374906786192dc
yamrire <yamrire@student.1337.ma>
2022-01-11 14:23:02 +0100

    Updating prototypes: return type void > int

--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 14:03:27 by yamrire           #+#    #+#             */
-/*   Updated: 2022/01/10 15:12:22 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/01/11 13:37:33 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-void	ft_putchar_fd(char c, int fd);
-void	ft_putnbr_fd(long n, int fd);
-void	ft_putnbrunsign_fd(unsigned long long n, int fd);
-void	ft_putstr_fd(char *s, int fd);
+int     ft_putchar_fd(char c, int fd);
+int	ft_putnbr_fd(long n, int fd);
+int	ft_putnbrunsign_fd(unsigned long long n, int fd);
+int	ft_putstr_fd(char *s, int fd);
 int     ft_printf(const char *str, ...);
-void    ft_nbr_base (unsigned long long n, int base, int);
+int    ft_nbr_base (unsigned long long n, int base, int);
 
 #endif
