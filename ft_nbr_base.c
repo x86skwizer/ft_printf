edf01ab0d2c79d71cd57f054812e5d3e7a184649
yamrire <yamrire@student.1337.ma>
2022-01-11 14:20:16 +0100

    Change the return type void > int

--- ft_nbr_base.c
+++ ft_nbr_base.c
@@ -6,20 +6,24 @@
 /*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 18:25:16 by yamrire           #+#    #+#             */
-/*   Updated: 2022/01/08 18:18:18 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/01/11 13:37:15 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-void ft_nbr_base (unsigned long long n, int base, int which)
+int ft_nbr_base (unsigned long long n, int base, int which)
 {
 	char *bb;
+	int	i;
+
+	i = 0;
 	if (which)
 		bb = "0123456789ABCDEF";
 	else
 		bb = "0123456789abcdef";
 	if (n / base)
 		ft_nbr_base (n / base, base, which);
-	ft_putchar_fd (bb[n % base], 1);
+	i += ft_putchar_fd (bb[n % base], 1);
+	return i;
 }
