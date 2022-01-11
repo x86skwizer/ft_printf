6c942002ac9f5447768f2cad78ee206bdf0475ec
yamrire <yamrire@student.1337.ma>
2022-01-11 14:20:30 +0100

    Change the return type void > int

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,13 +6,13 @@
 /*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 23:00:47 by yamrire           #+#    #+#             */
-/*   Updated: 2022/01/08 18:18:34 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/01/10 15:46:47 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-void	ft_putchar_fd(char c, int fd)
+int	ft_putchar_fd(char c, int fd)
 {
-	write(fd, &c, 1);
+	return (write(fd, &c, 1));
 }
