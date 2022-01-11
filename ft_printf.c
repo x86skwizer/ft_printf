70be85438a5617ef6491cbdfd317c87dce92eb55
yamrire <yamrire@student.1337.ma>
2022-01-11 14:24:07 +0100

    Fixing while(): return type int prob

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,12 @@
 /*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 01:50:45 by yamrire           #+#    #+#             */
-/*   Updated: 2022/01/10 12:38:21 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/01/11 14:18:25 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+#include <stdio.h>
 
 int getWhichHex(char c)
 {
@@ -19,51 +20,62 @@ int getWhichHex(char c)
 	return 0;
 }
 
-void handle_print_cases(const char *str, int i, va_list format)
+int	handle_print_cases(const char *str, int i, va_list format)
 {
-	unsigned int unsign;
+	unsigned int	unsign;
+	int				n;
 
+	n = 0;
 	unsign = 0;
-
 	if (str[i] == 'c')
-		ft_putchar_fd((char)va_arg(format, int), 1);
+		n = ft_putchar_fd((char)va_arg(format, int), 1);
 	else if (str[i] == 's')
-		ft_putstr_fd(va_arg(format, char *), 1);
+		n = ft_putstr_fd(va_arg(format, char *), 1);
 	else if (str[i] == 'd' || str[i] == 'i')
-		ft_putnbr_fd(va_arg(format, int), 1);
+		n = ft_putnbr_fd(va_arg(format, int), 1);
 	else if (str[i] == 'u')
 	{
 		unsign = va_arg(format, unsigned int);
-		ft_putnbrunsign_fd(unsign, 1);
+		n = ft_putnbrunsign_fd(unsign, 1);
 	}
 	else if (str[i] == 'x' || str[i] == 'X')
-		ft_nbr_base(va_arg(format, unsigned int), 16, getWhichHex(str[i]));
+		n = ft_nbr_base(va_arg(format, unsigned int), 16, getWhichHex(str[i]));
 	else if (str[i] == '%')
-		ft_putchar_fd('%', 1);
+		n = ft_putchar_fd('%', 1);
 	else if (str[i] == 'p')
 	{
-		ft_putstr_fd("0x", 1);
-		ft_nbr_base(va_arg(format, unsigned int), 16, 0);
+		n = ft_putstr_fd("0x", 1);
+		n += ft_nbr_base(va_arg(format, unsigned int), 16, 0);
 	}
+	return (n);
 }
 
 int ft_printf(const char *str, ...)
 {
 	int i;
+	int	n;
 	va_list format;
 
 	va_start(format, str);
 	i = 0;
+	n = 0;
 	while (str[i])
 	{
 		if (str[i] == '%')
 		{
 			i++;
-			handle_print_cases(str, i, format);
+			n += handle_print_cases(str, i , format);
 		}
 		else
-			ft_putchar_fd(str[i], 1);
+			n += ft_putchar_fd(str[i], 1);
 		i++;
 	}
-	return i;
+	va_end(format);
+	return (n);
 }
+
+// int	main(void)
+// {
+// 	ft_printf("l%cl%cl%cl%cl%c", 'a', 'i', 'u', 'e', 'o');
+// 	return 0;
+// }
