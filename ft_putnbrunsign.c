237fe9db7c44f4741f2f3c79e53ff409cca673db
yamrire <yamrire@student.1337.ma>
2022-01-15 22:17:40 +0100

    len from variable to param

--- ft_putnbrunsign.c
+++ ft_putnbrunsign.c
@@ -6,19 +6,16 @@
 /*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 18:18:52 by yamrire           #+#    #+#             */
-/*   Updated: 2022/01/11 15:34:18 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/01/15 22:10:13 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_putnbrunsign(unsigned long long n)
+int	ft_putnbrunsign(unsigned long long n, int *len)
 {
-	int	len;
-
-	len = 0;
 	if (n / 10)
-		ft_putnbrunsign(n / 10);
-	len += ft_putchar(n % 10 + '0');
-	return (len);
+		ft_putnbrunsign(n / 10, len);
+	*len += ft_putchar(n % 10 + '0');
+	return (*len);
 }
