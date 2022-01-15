602eafcdee85755fd57b96266d2fb683d2f3e8d0
yamrire <yamrire@student.1337.ma>
2022-01-15 22:17:25 +0100

    len from variable to param

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,30 +6,26 @@
 /*   By: yamrire <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 23:44:51 by yamrire           #+#    #+#             */
-/*   Updated: 2022/01/12 13:59:58 by yamrire          ###   ########.fr       */
+/*   Updated: 2022/01/15 22:14:49 by yamrire          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 
-int	ft_putnbr(long n)
+int	ft_putnbr(long n, int *len)
 {
-	int	len;
-
-	len = 0;
 	if (n < 0)
 	{
 		n *= -1;
-		len += ft_putchar('-');
+		*len += ft_putchar('-');
 	}
 	if (n)
 	{
 		if (n / 10)
-			ft_putnbr (n / 10);
-		len += ft_putchar (n % 10 + '0');
-		len++;
+			ft_putnbr (n / 10, len);
+		*len += ft_putchar (n % 10 + '0');
 	}
 	else
-		len += ft_putchar('0');
-	return (len);
+		*len += ft_putchar('0');
+	return (*len);
 }
